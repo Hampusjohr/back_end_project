0e190e9a2e0ff58a86aaf092d33a132ebba5c27f
Hampusjohr <hampusjohr@gmail.com>
2024-12-15 12:08:24 +0100

    removed unnessesary parts

--- project/restaurant/apps.py
+++ project/restaurant/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
-class RestaurantConfig(AppConfig):  # Ändra klassen till ett unikt namn
+class RestaurantConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'restaurant'  # Se till att namnet matchar appens nya mappnamn
+    name = 'restaurant'
