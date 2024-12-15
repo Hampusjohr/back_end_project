a9ac3b058cda8c57e2da2535873eeff975301d47
Hampusjohr <hampusjohr@gmail.com>
2024-12-15 18:29:15 +0100

    Models & admins done

--- LittleLemon/restaurant/models.py
+++ LittleLemon/restaurant/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+# Models
+class Menu(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+
+    class Meta:
+        db_table = 'menu'
+
+    def __str__(self):
+        return self.title
+
+
+class Booking(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField()
+
+    class Meta:
+        db_table = 'booking'
+
+    def __str__(self):
+        return f"Booking by {self.name} on {self.booking_date}"
+
