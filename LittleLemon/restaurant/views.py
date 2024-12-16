849817268d38d2f1b5a2b56cb26770544d33bb58
Hampusjohr <hampusjohr@gmail.com>
2024-12-16 12:25:19 +0100

    fixed Booking API so it can take bookings

--- LittleLemon/restaurant/views.py
+++ LittleLemon/restaurant/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .models import Menu
-from .serializers import MenuSerializer
+from rest_framework import generics, viewsets, permissions
+from .models import Menu,Booking
+from .serializers import MenuSerializer,BookingSerializer,UserSerializer
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
@@ -20,3 +21,12 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
     lookup_field = 'id'
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+   permission_classes = [permissions.IsAuthenticated]
